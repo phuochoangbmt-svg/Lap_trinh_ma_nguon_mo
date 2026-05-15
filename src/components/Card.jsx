5f01afcb2f4376085ae5313b2a8763ce91ba059a
phuochoangbmt-svg <phuochoangbmt@gmail.com>
2026-05-15 15:45:17 +0700

    update

--- src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-export const Card = ({ title, desc }) => {
-  return (
-    <div className="bg-white p-6 rounded-xl shadow hover:shadow-lg transition">
-      <h2 className="text-2xl font-bold text-blue-600 mb-2">{title}</h2>
-
-      <p className="text-gray-500">{desc}</p>
-    </div>
-  );
-};
